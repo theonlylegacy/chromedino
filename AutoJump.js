5da55d44113b0e0663e4e699cdcfd7a8f5c8d01e
Sero <159341819+theonlylegacy@users.noreply.github.com>
2025-01-05 21:32:37 +0100

    Create AutoJump.js

---
+++ AutoJump.js
@@ -0,0 +1,20 @@
+let Enabled = true
+
+const oldFunction = Runner.instance_.gameOver
+Runner.instance_.gameOver = function() {
+    if (Enabled) {
+        Runner.instance_.tRex.startJump(1)
+    } else {
+        oldFunction.call(Runner.instance_)
+    }
+}
+
+addEventListener("keydown", function(Input) {
+    if (Input.code == "KeyZ") {
+        if (Enabled) {
+            Enabled = false
+        } else {
+            Enabled = true
+        }
+    }
+})
